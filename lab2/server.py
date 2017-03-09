b1f73769e301903a7f7d985d3b2d9733ffd9ec64
Jonathan Hamel <jonathanhamel4@gmail.com>
2017-03-09 18:11:52 -0500

    Fixed server GET handling

--- lab2/server.py
+++ lab2/server.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import json
 import glob
+import time
 
 dir_path = os.path.dirname(os.path.realpath(__file__)) + "/data"
 
@@ -34,6 +35,7 @@ def handle_client(conn, addr):
             if method_tuple[0] == "GET":
                 print data
                 send_get_data(headers, conn, method_tuple[1])
+                break;
 
     finally:
         conn.close()
@@ -51,18 +53,35 @@ def send_get_data(headers, conn, path):
             filename = glob.glob(os.path.join(dir_path, path + '.*'))[0]
             print filename
             with open(filename, 'r') as myfile:
-                data = 'HTTP/1.0 200 OK\r\n'
-                data = data + "Content-Type: text/plain\r\n\r\n"
-                data = data + myfile.read()
+                file_data = myfile.read()
+                file_length = len(file_data)
+                data = make_return_header(True, file_length, "text/plain")
+                data += file_data + "\r\n\r\n"
         except IndexError, e:
-            data = 'HTTP/1.0 404 Not Found\r\n'
+            data = make_return_header(False, 0, "")
     else:
-        data = 'HTTP/1.0 200 OK\r\n'
-        data = data + "Content-Type: application/json\r\n\r\n"
-        data += json.dumps(os.listdir(dir_path))
+        file_data = json.dumps(os.listdir(dir_path))
+        file_length = len(file_data)
+        data = make_return_header(True, file_length, "text/plain")
+        data += file_data + "\r\n\r\n"
     conn.sendall(data)
 
+def make_return_header(ok, length, content_type):
+    header_string = ""
+    if ok is True:
+        header_string += "HTTP/1.0 200 OK\r\n"
+    else:
+        header_string += "HTTP/1.0 404 Not Found\r\n"
 
+    current_date = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+    header_string += "Date: " + current_date + "\r\n"
+    header_string += "Server: Simple Python Server\r\n"
+
+    if length > 0:
+        header_string += "Content-Type: " + content_type + "\r\n"
+        header_string += "Content-Length: " + str(length) + "\r\n"
+    header_string += "\r\n"
+    return header_string
 
 # Usage python echoserver.py [--port port-number]
 parser = argparse.ArgumentParser()
