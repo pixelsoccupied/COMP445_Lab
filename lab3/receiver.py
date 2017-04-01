b79029f73a417a887d7369eed76f75238d0d6b94
Jonathan Hamel <jonathanhamel4@gmail.com>
2017-03-31 22:09:00 -0400

    Added PRIVATE-TALK

--- lab3/receiver.py
+++ lab3/receiver.py
@@ -12,53 +12,71 @@ class Receiver(Thread):
         self.users = []
 
     def run(self):
-        # print "Receiver using: " + self.address + ":" + str(self.port) + "\n"
         receiverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         receiverSocket.bind((self.address, self.port))
         self.socket_address = socket.gethostbyname(socket.gethostname())
-        # print self.socket_address
 
         while self.thread_on == True:
             message, clientAddress = receiverSocket.recvfrom(2048)
-            # print message
-            # print clientAddress
             message_content = self.split_message(message)
-            modifiedMessage = self.parse_message(message_content)
+            modifiedMessage = self.parse_message(receiverSocket, message_content, clientAddress)
 
             if modifiedMessage is not None:
-                # print message_content[1]
                 print modifiedMessage
             elif message_content[1] == "QUIT":
                 self.thread_on = False
-        # print "Closing Receiver"
 
     def split_message(self, message):
-        # print message
         message_split = message.split("\n")
-        user_name = message_split[0].split(":")[1]
+        user_name = message_split[0].split(":")[1:]
         command = message_split[1].split(":")[1]
         message_content = message_split[2].split(":")[1]
         return (user_name, command, message_content)
 
-    def parse_message(self, message_content):
+    def parse_message(self, receiverSocket, message_content, ip_address):
         now = datetime.datetime.now()
         command = message_content[1]
         if command == "JOIN":
-            return str(now) + " " + message_content[0] + " joined!"
+            return str(now) + " " + message_content[0][0] + " joined!"
         elif command == "TALK":
-            return str(now) + " [" + message_content[0] + "]: " + message_content[2]
+            return str(now) + " [" + message_content[0][0] + "]: " + message_content[2]
         elif command == "LEAVE":
-            try:
-                self.users.remove(message_content[0])
-            except ValueError, e:
-                pass
-            finally:
-                return str(now) + " " + message_content[0] + " leaved!"
+            self.remove_from_users(ip_address)
+            return str(now) + " " + message_content[0][0] + " leaved!"
         elif command == "WHO":
-            return str(now) + " Connected users: " + str(self.users)
+            return str(now) + " Connected users: " + str([i[1] for i in self.users])
         elif command == "PING":
-            # print "Adding: " + message_content[0]
-            self.users.append(message_content[0])
+            self.users.append((ip_address, message_content[0][0]))
             return None
+        elif command == "PRIVATE-TALK" and ip_address[0] == self.socket_address:
+            user = self.find_user_by_name(message_content[0][1])
+            self.send_to_private_user(now, receiverSocket, user[0], message_content)
+            return None
+        elif command == "PRIVATE-TALK" and ip_address[0] != self.socket_address:
+            return str(now) + " [" + message_content[0][0] + "] (PRIVATE): " + message_content[2]
         else:
             return None
+
+    def remove_from_users(self, ip_address):
+        for user in self.users:
+            if user[0] == ip_address:
+                self.users.remove(user)
+                break
+
+    def find_user_by_name(self, username):
+        userfound = None
+        for user in self.users:
+            if user[1] == username:
+                userfound = user
+                break
+        return userfound
+
+    def send_to_private_user(self, now, receiverSocket, ipaddress, message_content):
+        message = self.build_message(message_content[0][0], message_content[2], message_content[1])
+        receiverSocket.sendto(message, (ipaddress[0], self.port));
+
+    def build_message(self, username, message, command):
+        user_message = "user:" + username + "\n"
+        user_message += "command:" + command + "\n"
+        user_message += "message:" + message + "\n\n"
+        return user_message
--- lab3/sender.py
+++ lab3/sender.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import socket
 from threading import Thread
+import re
 
 class Sender(Thread):
 
@@ -11,10 +12,10 @@ class Sender(Thread):
         self.thread_on = True
 
     def run(self):
-        #print "Sender using: " + self.address + ":" + str(self.port) + "\n"
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.socket_address = socket.gethostbyname(socket.gethostname())
         username = raw_input('Enter your name: ')
         self.send_message(clientSocket, "", username, "JOIN")
         self.send_message(clientSocket, "", username, "PING")
@@ -27,23 +28,25 @@ class Sender(Thread):
                 self.send_message(clientSocket, "", username, "QUIT")
             elif message == "/who":
                 self.send_message(clientSocket, "", username, "WHO")
+            elif re.match(r'^\/private\s.*', message) is not None:
+                user_to_send = message.replace("/private ", "").strip()
+                message_to_send = raw_input('Private message to send to ' + user_to_send + ": ")
+                user_string = username + ":" + user_to_send
+                self.send_message(clientSocket, message_to_send, user_string, "PRIVATE-TALK")
             else:
                 self.send_message(clientSocket, message, username, "TALK")
-            # serverMessage, serverAddress = clientSocket.recvfrom(2048)
-            # print serverMessage
-        # print "Closing Sender"
         clientSocket.close()
 
     def send_message(self, clientSocket, message, username, command):
         messageaddress = self.address
-        if command == "WHO" or command == "QUIT":
-            messageaddress = "127.0.0.1"
+        if command == "WHO" or command == "QUIT" or command == "PRIVATE-TALK":
+            messageaddress = self.socket_address
 
         modifiedMessage = self.build_message(username, message, command)
         clientSocket.sendto(modifiedMessage,(messageaddress, self.port))
 
     def build_message(self, username, message, command):
-        user_message = "user:" + username + "\n"
-        user_message += "command:" + command + "\n"
+        user_message = "user:" + username
+        user_message += "\ncommand:" + command + "\n"
         user_message += "message:" + message + "\n\n"
         return user_message
